9779629215bbdf4cc0dc40a87423fa56a4273ad1
Martin Koroudjiev <martin@prosveta.bg>
2022-05-06 09:21:53 +0300

    Signed-off-by: Martin Koroudjiev <martin@prosveta.bg>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,22 @@
-module.exports = {
-  apps : [{
-    name: 'DdosDetector',
-    script: 'app.js',
-    args: '--path-watch=/data/prosveta/logs/access/all-*.log --block-ips-file=/data/prosveta/log-parser/blockips.conf',
-    instances: '1',
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '16G',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    output: '/dev/null',
-    error:  '/dev/null',
-    log:    '/data/prosveta/logs/ddos-detector/app.log',
-    merge_logs: true,
-    time: true,
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }]
-};
+export const apps = [{
+        name: 'DdosDetector',
+        script: 'app.js',
+        args: '--path-watch=/data/prosveta/logs/access/all-*.log --block-ips-file=/data/prosveta/log-parser/blockips.conf',
+        instances: '1',
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '16G',
+        log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        output: '/dev/null',
+        error: '/dev/null',
+        log: '/data/prosveta/logs/ddos-detector/app.log',
+        merge_logs: true,
+        time: true,
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }]
+;

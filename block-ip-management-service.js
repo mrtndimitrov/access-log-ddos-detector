f1ba496bda1fc3dd977e5d738e9865dd269af0c2
mrtnd <mrtndimitrov@gmail.com>
2024-04-10 08:58:03 +0300

    Signed-off-by: mrtnd <mrtndimitrov@gmail.com>

---
+++ block-ip-management-service.js
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import { DbService } from './db-service.js';
+import { EOL } from 'os';
+
+export class BlockIpManagementService {
+    blockIpsFile = null;
+    db = null;
+    ips = [];
+
+    constructor(argv) {
+        this.blockIpsFile = argv['block-ips-file'] ? argv['block-ips-file'] : (argv['b'] ? argv['b'] : null);
+        if (this.blockIpsFile && fs.existsSync(this.blockIpsFile)) {
+            this._processBlockFile();
+        }
+    }
+    async _processBlockFile(){
+        this.db = await DbService.getInstance();
+        const allFileContents = fs.readFileSync(this.blockIpsFile, 'utf-8');
+        this.ips = allFileContents.split(/\r?\n/);
+        for (const ip of this.ips) {
+            console.log(`IP ${ip} found in BLOCK file. Adding it in our DB.`);
+            let ipInfo = await this.db.getIpInfo(ip);
+            if(!ipInfo){
+                ipInfo = {
+                    ip: ip,
+                    checked: 0,
+                    blocked: true,
+                    allowed: false,
+                    suspicious: 0
+                }
+                await this.db.insertIpInfo(ipInfo);
+            } else {
+                await this.db.updateIpInfo(ipInfo._id, {blocked: true, allowed: false});
+            }
+        }
+    }
+
+    async block(ip) {
+        if (this.blockIpsFile && fs.existsSync(this.blockIpsFile)) {
+            if (this.ips.indexOf(ip) === -1) {
+                // let's reload the file first in case someone has modified it manually
+                await this._processBlockFile();
+                console.log(`Adding IP ${ip} to block file.`);
+                this.ips.push(ip);
+                fs.writeFileSync(this.blockIpsFile, this.ips.join(EOL));
+            }
+        }
+    }
+    async unblock(ip) {
+        if (this.blockIpsFile && fs.existsSync(this.blockIpsFile)) {
+            const index = this.ips.indexOf(ip);
+            if (index > -1) {
+                // let's reload the file first in case someone has modified it manually
+                await this._processBlockFile();
+                console.log(`Removing IP ${ip} from block file.`);
+                this.ips.splice(index, 1);
+                fs.writeFileSync(this.blockIpsFile, this.ips.join(EOL));
+            }
+        }
+    }
+}

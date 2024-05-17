276be9d04441b54d30ffd55f9301ae90029b9619
mrtnd <mrtndimitrov@gmail.com>
2024-05-17 15:20:12 +0300

    Signed-off-by: mrtnd <mrtndimitrov@gmail.com>

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,23 +1,23 @@
 module.exports = {
-        apps: [{
-                name: 'DdosDetector',
-                script: 'app.js',
-                args: '--NOT-USED-path-watch=/data5/prosveta/logs/access/all-*.log --block-ips-file=/prosveta/blocked-ips.conf --allow-ips-file=/prosveta/allowed-ips.conf',
-                instances: '1',
-                autorestart: true,
-                watch: false,
-                max_memory_restart: '16G',
-                log_date_format: 'YYYY-MM-DD HH:mm:ss',
-                output: '/dev/null',
-                error: '/dev/null',
-                log: '/prosveta/logs/ddos-detector/app.log',
-                merge_logs: true,
-                time: true,
-                env: {
-                        NODE_ENV: 'development'
-                },
-                env_production: {
-                        NODE_ENV: 'production'
-                }
-        }]
+    apps: [{
+        name: 'DdosDetector',
+        script: 'app.js',
+        args: '--NOT-USED-path-watch=/data5/prosveta/logs/access/all-*.log --block-ips-file=/prosveta/blocked-ips.conf --allow-ips-file=/prosveta/allowed-ips.conf',
+        instances: 'max',
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '16G',
+        log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        output: '/dev/null',
+        error: '/dev/null',
+        log: '/prosveta/logs/ddos-detector/app.log',
+        merge_logs: true,
+        time: true,
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }]
 };
